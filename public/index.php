c59e20ef7d82e8f5ff8aa196a33b4be282c69108
Yaowen Zhang <yaowen.zhang@live.com>
2016-09-10 09:48:27 +0800

    update

--- public/index.php
+++ public/index.php
@@ -26,5 +26,8 @@ require __DIR__ . '/../src/middleware.php';
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
+// Help functions
+require __DIR__ . '/../src/helpers.php';
+
 // Run app
 $app->run();
--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,10 @@ $app->get('/', function ($request, $response, $args) {
     // Sample log message
     //$this->logger->info("Slim-Skeleton '/' route");
 
+	if(is_mobile()) {
+		return $response->withHeader('Location',  'http://z.maka.im/k/02B5R8BA');
+	}
+	
     // Render index view
     return $this->view->render($response, 'index.twig.php', $args);
 });
